f3e1f05510f1607848b2e310c3e5a9a1db564b58
Dominik Eichler <dspam@ldkf.de>
2016-01-20 20:48:42 +0100

    config

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,7 +1,6 @@
 <?php
   	include("config.php");
 	$server="localhost";
-	$dbname="vudb";
 	$verbindung = mysql_connect($server, $username , $password) or die("Verbindung zur Datenbank konnte nicht hergestellt werden"); 
 	mysql_select_db($dbname) or die ("Datenbank konnte nicht ausgewählt werden"); 
 ?>
