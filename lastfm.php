a8cf6e6f23f1800cc8c95be2c0f26f85bc53454d
Dominik Eichler <dspam@ldkf.de>
2016-01-21 16:10:57 +0100

    added login

--- lastfm.php
+++ lastfm.php
@@ -80,7 +80,7 @@
          	$_SESSION['user']=$uname_db;
          	$_SESSION['sk']=$sk;
 				$_SESSION['sig']=$sig;
-				if(!isset($_COOKIE[$user_in]) and isset($_POST['start']) and $_POST['start']==1) {
+				if(!isset($_COOKIE''.[$user_in.''])) {
 					setcookie($user_in, $sig, time()+(3600*24*365));  
 				}  
         	}
