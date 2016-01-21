da8dfdc7f7e89a82280cea7a341bb91a4ff0cfea
Dominik Eichler <dspam@ldkf.de>
2016-01-21 20:18:42 +0100

    running

--- include/functions.php
+++ include/functions.php
@@ -192,6 +192,8 @@
 		while($getplaces = mysql_fetch_row($getplace)){
 			$places[]=$getplaces[0];
 		}
+		
+	/*	
 		$getmembers = mysql_query("SELECT `username` FROM `ldkf_lastfm`"); 
 		$l=0;
 		while($members = mysql_fetch_row($getmembers)){
@@ -205,9 +207,10 @@
 			<input type="hidden" name="username" value="'.$member_name.'">
 			<input type="hidden" name="method" value="2">
 			<button type="submit" class="userButton">'.$member_name.'</button></form>';
-		}
-		$content .='</div>
-		</div>
+		}*/
+		$content .='		
+		
+		
  		<table style="border-top:2px solid; border-left:2px solid;">
  		<tbody>
  			<tr>
