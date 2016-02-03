ff7a56281c5e5157707083ce1d19a457a42dea72
Dominik Eichler <dspam@ldkf.de>
2016-02-03 15:44:12 +0100

    login

--- lastfm.php
+++ lastfm.php
@@ -20,8 +20,8 @@
 			$uname_db=$_SESSION['user'];
 		}
 		elseif (isset($_GET['logout'])){
-			$user_in=$_GET['user'];
-			logout($user_in);
+			$user_in=$_SESSION['user'];
+			logout($user_in, $db);
 			$method_in=$_GET['methodlogout'];
 			$uname_db=$user_in;
 		}
