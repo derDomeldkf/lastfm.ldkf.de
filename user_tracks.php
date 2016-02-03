a8511188ce09c9a4ad2bc3215e672d95cc2f20f0
Dominik Eichler <dspam@ldkf.de>
2016-02-03 17:09:52 +0100

    login

--- user_tracks.php
+++ user_tracks.php
@@ -86,7 +86,7 @@
       						'; 
          					if($love==1) {
 									echo '
-										<a href="include/do.php?f=unlove?artist='. urlencode($artist_name) .'&track='. urlencode($track_name) .'" style="padding:1px; margin:0;">
+										<a href="include/do.php?f=unlove&artist='. urlencode($artist_name) .'&track='. urlencode($track_name) .'" style="padding:1px; margin:0;">
 											<img width="18px" height="18px;" src="pic/love.png">
 										</a>
 									';
