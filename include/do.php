24513c94ba8b4cd666f73cb14dfeee5e61d1422f
Dominik Eichler <dspam@ldkf.de>
2016-02-03 17:08:02 +0100

    login

--- include/do.php
+++ include/do.php
@@ -8,5 +8,6 @@
 		$methode="method=track.unlove&track=".$track."&artist=".$artist."&api_sig".$sig."&sk=".$sk;
 		$out_user = file_get_contents("https://ws.audioscrobbler.com/2.0/?format=json&api_key=830d6e2d4d737d56aa1f94f717a477df&" . $methode);
 		echo $out_user;
+		echo $methode;
 	}
 ?> 
