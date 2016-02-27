97229d8064d46ffdd6d3f33770c0db887ebb2b4a
Dominik Eichler <dspam@ldkf.de>
2016-02-27 14:06:23 +0100

    updates

--- include/functions.php
+++ include/functions.php
@@ -130,6 +130,18 @@
  		while ($getuser = $getusers->fetch_assoc()) {
  			$users[]=$getuser['username'];
  		}		
+ 		$sql = "CREATE TABLE `".$db_name."` (
+			playcount INT(3) NOT NULL,
+			artist VARCHAR(40) NOT NULL COLLATE utf8_general_mysql500_ci,
+			user TEXT NOT NULL COLLATE latin1_swedish_ci
+		)";
+		if ($db->query($sql) === TRUE) {
+			$s=0;
+		}
+		else {
+   	 	echo "Error creating table: " . $db->error;
+   	 	$s=1;
+		}	
 		$delete = $db->query("DELETE FROM ".$db_name);
 		$d=0;
 		foreach($users as $user_in){
@@ -182,18 +194,6 @@
  		while ($getuser = $getusers->fetch_assoc()) {
  			$users[]=$getuser['username'];
  		}	
- 		$sql = "CREATE TABLE `".$db_name."` (
-			playcount INT(3) NOT NULL,
-			artist VARCHAR(40) NOT NULL COLLATE utf8_general_mysql500_ci,
-			user TEXT NOT NULL COLLATE latin1_swedish_ci
-		)";
-		if ($db->query($sql) === TRUE) {
-			$s=0;
-		}
-		else {
-   	 	echo "Error creating table: " . $db->error;
-   	 	$s=1;
-		}	
 		$d=0;
 		foreach($users as $user_in){
 			$methode="method=".$command."&user=".$user_in;
--- refresh_tracks.php
+++ refresh_tracks.php
@@ -5,17 +5,16 @@
  	include "include/functions.php";	
  	
 #####################################
-	$db_t = date('YW');
-	$db_name=$db_t."_titel";
-	$insert = $db->query("INSERT INTO tables_title (table_name) VALUES ('$db_name')"); 
-	$command="user.getTopArtists&period=7day"; 	
+	$db_name="last_fm_charts_track";
 	$command="user.getTopTracks&limit=40&period=overall";
  	refresh3($db_name, $command, $db); 
  	
 ######################################	
+	$db_t = date('YW');
+	$db_name=$db_t."_titel";
+	$insert = $db->query("INSERT INTO tables_title (table_name) VALUES ('$db_name')"); 
 	$time=	time();
 	$time2=$time-(3600*24*7);
-	$db_name="last_fm_charts_track";
 	$command="user.getWeeklyTrackChart&from=".$time2."&to=".$time; 	
  	refresh2($db_name, $command, $db); 	
  	
