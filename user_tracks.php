6f45a08638c0e27fc0cd4f1afff15b42b48f4d1b
Dominik Eichler <dspam@ldkf.de>
2016-03-06 21:11:59 +0100

    updates

--- user_tracks.php
+++ user_tracks.php
@@ -73,7 +73,7 @@
 				if($date_decode!="wird gerade gehört") {
 					$date_uts=$date_array['uts']+3600;  //lastfm fehler ausgleichen
 				}
-				//$images=$image_array['#text'];
+				$images=$image_array['#text'];
 				//$images="";
 				$image=image($images, $artist_name, $db, $album_name);
 				if($date_decode!="wird gerade gehört") {
