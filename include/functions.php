5e4c16fa563e2ed977f7608c7c5acc2d1231c379
Dominik Eichler <dspam@ldkf.de>
2016-03-08 20:30:06 +0100

    add scrobble function

--- include/functions.php
+++ include/functions.php
@@ -726,8 +726,8 @@
 					$album = $getinfo->fetch_assoc()['name'];
 					$sk=$_SESSION['session'];
 					$sig=$_SESSION['sig'];
-					
-					$methode="method=track.love&artist=" .$artist. "&sk=" .$sk. "&timestamp=" . time() . "&track=" .$track. "&api_sig=" .$sig;
+					$methode="method=track.scrobble&track=".$track."&artist=".$artist."&api_sig=".$sig."&sk=".$sk. "&timestamp=" . time() ;
+
 					if(isset($album) and $album!="") {
 						$methode .="&album=". urlencode($album);
 					}
