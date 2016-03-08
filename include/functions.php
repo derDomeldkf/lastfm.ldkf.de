d7349c370bb0a6d85228b9a49bc0fd5ec8e2d03c
Dominik Eichler <dspam@ldkf.de>
2016-03-08 21:11:27 +0100

    add scrobble function

--- include/functions.php
+++ include/functions.php
@@ -727,7 +727,7 @@
 					$sk=$_SESSION['session'];
 					//$sig=$_SESSION['sig'];
 					$sig=md5("album". $album ."api_key830d6e2d4d737d56aa1f94f717a477dfartist". $artist ."methodtrack.scrobblesk".$sk."timestamp". time() ."track". $track ."".$secret);
-					$methode="method=track.scrobble&track=".$track."&artist=".$artist."&api_sig".$sig."&sk=".$sk."&timestamp=" . time() ;
+					$methode="method=track.scrobble&track=".$track."&artist=".$artist."&api_sig=".$sig."&sk=".$sk."&timestamp=" . time() ;
 					if(isset($album) and $album!="") {
 						$methode .="&album=". $album;
 						
