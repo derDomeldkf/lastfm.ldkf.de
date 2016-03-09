bfcad8902fae529f9f3116ff86fc6d26cae3cc0a
Dominik Eichler <dspam@ldkf.de>
2016-03-09 15:56:14 +0100

    update

--- include/functions.php
+++ include/functions.php
@@ -2,7 +2,8 @@
 	function play($track_name, $artist_name, $db, $method_in, $limit_in, $page_in){
 		$content="";
 		if(isset($_SESSION['user'])) {
-			$track_name_get=utf8_decode($track_name);
+			$track_name_get =  str_replace("'", "&#180;",$track_name);
+			$track_name_get=utf8_decode($track_name_get);
 			$artist_name_get=utf8_decode($artist_name);
 			$gettrack = $db->query("SELECT `id` FROM `track` WHERE name LIKE '$track_name_get'"); 
 			if(isset($gettrack->num_rows) and  $gettrack->num_rows!= 0) {
