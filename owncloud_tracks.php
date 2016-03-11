a49e67d1a8b1da633e81641ef937a67ffed2980d
Dominik Eichler <dspam@ldkf.de>
2016-03-11 14:24:28 +0100

    fixes

--- owncloud_tracks.php
+++ owncloud_tracks.php
@@ -60,11 +60,11 @@
 						$track= !empty($ThisFileInfo['comments_html']['title']) ? $ThisFileInfo['comments_html']['title'][0] : "";
 						$time= !empty($ThisFileInfo['playtime_string']) ? $ThisFileInfo['playtime_string'] : "";
 						if($track!="") {
-							$track=mysql_escape_string(utf8_encode(rep($track))  );
+							$track=mysql_escape_string(utf8_encode(($track)) );
 							
 							echo $track;
- 							$artist=mysql_escape_string(utf8_encode(rep($artist)));
-							$album=mysql_escape_string(utf8_encode(rep($album)));	
+ 							$artist=mysql_escape_string(utf8_encode(($artist)));
+							$album=mysql_escape_string(utf8_encode(($album)));	
 							insert_info($path_in, $artist, $album, $track, $time, $db);
 						}
 					}
