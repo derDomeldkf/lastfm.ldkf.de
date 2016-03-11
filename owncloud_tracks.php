a5ff43a04a1352a6674cd9b4d96f0db866125a7f
Dominik Eichler <dspam@ldkf.de>
2016-03-11 14:48:45 +0100

    fixes

--- owncloud_tracks.php
+++ owncloud_tracks.php
@@ -40,6 +40,7 @@
 		$data =  str_replace("&#246;", "ö",$data);
 		$data =  str_replace("&#228;", "ä",$data);
 		$data =  str_replace("&amp;", "&",$data);
+		$data =  str_replace("´", "'",$data);
 		$data=htmlentities($data, ENT_QUOTES);
 		return $data;
 	
