66d3cbdaa62b9fc5efccc546738d3ad862dd63c5
Dominik Eichler <dspam@ldkf.de>
2016-03-12 15:50:15 +0100

    fixed navbar when playing

--- lastfm.php
+++ lastfm.php
@@ -209,7 +209,7 @@
 	</head>
 	<body style="font-family: ubuntu-m;">
 		<div id="content" class="main-content" role="main" style="">
-			<nav <?php echo 'class="navbar navbar-inverse'; echo (isset($_GET['p']) and $_GET['p']!='') ? ' navbar-fixed-top" style="margin-bottom:70px;' : ' navbar-static-top'; ?> ">
+			<nav <?php echo 'class="navbar navbar-inverse'; echo (isset($_GET['p']) and $_GET['p']!='') ? ' navbar-fixed-top' : ' navbar-static-top'; ?> ">
 	  			<div class="container-fluid" id="navigation" style="display:block;">
     			<!-- Brand and toggle get grouped for better mobile display -->
     				<div class="navbar-header">
@@ -230,7 +230,7 @@
 	  				</div>
   				</div>
 			</nav>
-				<div class="main" style="margin-left:0; padding-bottom:70px;">
+				<div class="main" <?php echo 'style="margin-left:0; padding-bottom:70px; '; echo (isset($_GET['p']) and $_GET['p']!='') ? ' margin-top:50px;' : ''; ?> ">
 					<section class="tracklist-section">
 						<?php 
 							switch($method_in) {
