b69b84af781ea840e34a984646bdf7d25ded466b
Dominik Eichler <dspam@ldkf.de>
2016-03-12 12:44:07 +0100

    split functions

---
+++ include/group.functions.php
@@ -0,0 +1,277 @@
+<?php
+
+	function group($db_name, $period, $db, $post, $date, $td) {
+ 		$content="";
+ 		if(!isset($_POST['userselc'][0])) {
+			$getplace = $db->query("SELECT `artist` FROM `".$db_name."` ORDER BY playcount DESC"); 
+			$user_input[]="0";
+		}
+		else {
+			$i=0;
+			$where="";
+			foreach($_POST['userselc'] as $user_sel) {
+				if($i==0) {
+					$where .="WHERE user LIKE '%".$user_sel."%'";
+					$user_input[]=$user_sel;
+				
+				}
+				else {
+					$where .="and user LIKE '%".$user_sel."%'";
+					$user_input[]=$user_sel;
+				}
+			$i++;
+			}
+			$getplace = $db->query("SELECT `artist` FROM `".$db_name."` ".$where." ORDER BY playcount DESC"); 
+		}		
+		
+		while($getplaces = $getplace->fetch_assoc()){
+			$places[]=$getplaces['artist'];
+		}
+		if(!isset($places) and isset($ua)) {
+			$content .='
+				<div class="row" style="margin:0">
+					<div class="col-md-9" style="padding-left:40px;">
+						<h3>Diese Benutzer habe keine gemeinsam gehörten Künstler.</h3>
+			';		
+		}
+		else {
+		$content .='		
+		<div class="row" style="margin:0">
+		<div class="col-md-9" style="padding-left:40px;">
+ 		<table style="">
+ 		<tbody>
+ 			<tr>
+ 				'.$td.'>
+ 				</td>
+				'.$td.' style="padding-left:10px;">
+					Platz
+				</td>
+				'.$td.'>		
+				</td> 
+				'.$td.' style="padding-left:8px;">
+					K&uuml;nstler
+				</td>
+				'.$td.'>
+					'.$period.'				
+				</td> 	
+			</tr>';
+		$i=0;	 
+		$place=1;	
+		if(isset($places )) {
+		foreach($places as $artist_name){
+			$getartist = $db->query("SELECT `playcount` FROM `".$db_name."` WHERE artist LIKE '$artist_name'"); 
+			$counter = $getartist->fetch_assoc();
+			$count=$counter['playcount'];
+			if($place==1) {
+				$count_max=$count;
+			}
+			$getuser = $db->query("SELECT `user` FROM `".$db_name."` WHERE artist LIKE '$artist_name' "); 
+			$users_names= $getuser->fetch_assoc()['user'];
+			$user=listeners_dropdown($users_names);
+			if($count>1) {
+			$content .='
+				<tr class="" style="';
+				if($i==0) { 
+					$content .='background-color: #F2F2F2;';
+				}
+				$content .='">';
+				$content .= image_artist($artist_name, $db); 				
+				$content .='
+					<td class="list" style="padding-left:15px;">
+  	 	        		<span class="">
+  	    	   			<span class="chartlist-image">
+  	       					'.$place.'
+ 	       				</span>
+ 		  	 			</span>         		
+ 	     	  		</td>
+ 	     	  		<td class="list" style="padding-left:8px; ">
+  	  					<span>('.$count.')</span>
+ 	         	</td>     
+ 	   			<td class="chartlist-ellipsis-wrap list" style="padding-left:10px; padding-right:4px; min-width:260px;">
+   	   			<span class="chartlist-ellipsis-wrap">
+  							<a href="http://www.last.fm/music/'.$artist_name.'" target="_blank">'.$artist_name.'</a>
+ 	  	 				</span>
+					</td>
+  	  				<td class="list" style="padding-right:3px; min-width:360px;">
+ 	   				<span>'.$user.'</span>
+ 	           	</td>
+				</tr>';
+			if($i==0){$i++;}
+ 	     else {$i--;}
+ 	  	}	
+ 	     $place++;
+ 	    }
+		}
+		
+	 	$content .= '
+ 			</tbody>
+		</table>';
+		}
+		$content .= '
+		</div>
+		<div class="col-md-3" style="padding-left:30px; padding-top:20px;">
+		';
+		if($db_name=="last_fm_charts_all") {
+			$getmembers = $db->query("SELECT `username` FROM `ldkf_lastfm` order by `username` ASC"); 
+				while($members = $getmembers->fetch_assoc()){
+					$member[]=$members['username'];
+				}
+			$content .= '
+			<h4>Gemeinsame Künstler von:</h4>
+				<div style="max-width:220px; margin-top:30px;">
+					<form class="form-signin" method="post" action="lastfm.php?">
+   					<input type="hidden" name="method" value="8">
+   					<select name="userselc[]" class="form-control" style="margin-bottom:5px;" required>';
+   					$content .= select($member, $user_input, "0");
+   					$content .= '
+   					<select name="userselc[]" class="form-control" style="margin-bottom:5px;" required>';
+   					$content .= select($member, $user_input, "1");
+   					$content .= '
+   					<select name="userselc[]" class="form-control" style="margin-bottom:5px;">';
+   					$content .= select($member, $user_input, "2");
+   					$content .= '
+						<select name="userselc[]" class="form-control" style="margin-bottom:5px;">';
+   					$content .= select($member, $user_input, "3");
+ 						$content .= '
+   					<br>
+						<button type="submit" class="btn btn-primary">
+							Suchen
+						</button>
+   				</form>';
+   				if(isset($_POST['userselc'][0]) and $_POST['userselc'][0] != "") {
+   					$content .= '
+   				<form class="form-signin" method="post" action="lastfm.php?">
+   					<input type="hidden" name="method" value="8">
+						<button type="submit" class="btn btn-primary">
+							Reset
+						</button>
+   				</form>';
+   				}
+   			$content .= '
+   			</div>	
+			';
+		}
+		else {
+			$getid = $db->query("SELECT id FROM `tables` ORDER BY id DESC"); 
+			while($getplaces = $getid->fetch_assoc()){
+				$ids[]=$getplaces['id']; 
+			}
+			$content .= '<h4>'.$date.'</h4>
+				<div style="max-width:220px; margin-top:30px;">
+			';
+			$i=1;
+			foreach($ids as $id){
+				$content .= '
+					<form class="form-signin" method="post" action="lastfm.php?">
+				  		<input type="hidden" name="tableselect" value="'.$id.'">
+				  		<input type="hidden" name="method" value="4">
+   					<button type="submit" class="btn btn-primary"'; if((isset($_POST['tableselect']) and $id==$_POST['tableselect']) or ($post==$id)) {  $content .= " disabled";} $content .= '>
+							Seite '.$i.'
+						</button>
+   				</form>
+   			';
+   			$i++;
+			}
+			$content .= '
+   			</div>	
+			';
+		}	
+				
+		$content .= '
+		</div>
+		</div>
+		';
+		return $content;
+	}
+
+##################################################################################################################################
+	function group2($db_name, $period, $db, $method_in, $td) {
+		
+ 		$content=head();
+		$getplace = $db->query("SELECT `titel` FROM ".$db_name." ORDER BY playcount DESC "); 
+		while($getplaces = $getplace->fetch_assoc()){
+			$places[]=$getplaces['titel'];
+		}
+		$content .='
+ 			<tr>
+ 				'.$td.'>
+ 				</td>
+				'.$td.' style="padding-left:10px;">
+					Platz
+				</td>
+				'.$td.'>				
+				</td> 
+				'.$td.' style="padding-left:8px;">
+					K&uuml;nstler — Titel
+				</td>
+				'.$td.'>
+				</td>
+				'.$td.'>
+				</td>
+				'.$td.'>
+					'.$period.'				
+				</td> 	
+			</tr>';
+		$i=0;	 
+		$place=1;		
+		foreach($places as $track_name){
+			$getartist = $db->query("SELECT `playcount` FROM ".$db_name." WHERE titel LIKE '$track_name'"); 
+			$count = $getartist->fetch_assoc()['playcount'];
+			if($place==1) {
+				$count_max=$count;
+			}
+			$getuser = $db->query("SELECT `user` FROM ".$db_name." WHERE titel LIKE '$track_name' "); 
+			$users_names = $getuser->fetch_assoc()['user'];
+			$getart= $db->query("SELECT `artist` FROM ".$db_name." WHERE titel LIKE '$track_name' "); 
+			$artist_name= $getart->fetch_assoc()['artist'];
+			$user=listeners_dropdown($users_names);
+			if($count>1) {
+			$content .='
+				<tr class="" style="';
+				if($i==0) { 
+					$content .='background-color: #F2F2F2;';
+				}
+				$content .='">';
+				$content .= image_artist($artist_name, $db); 				
+				$content .='
+					<td class="list" style="padding-left:15px;">
+  	 	        		<span class="">
+  	    	   			<span class="chartlist-image">
+  	       					'.$place.'
+ 	       				</span>
+ 		  	 			</span>         		
+ 	     	  		</td>
+ 	     	  		<td class="list" style="padding-left:8px; ">
+  	  					<span>('.$count.')</span>
+ 	         	</td>     
+ 	   			<td class="chartlist-ellipsis-wrap list" style="padding-left:10px; padding-right:4px; min-width:460px;">
+   	   			<span class="chartlist-ellipsis-wrap">
+      	   			<span class="chartlist-artists">
+      	   				<a href="http://www.last.fm/music/'.$artist_name.'" target="_blank">'.$artist_name.'</a>
+         				</span>
+							<span class="artist-name-spacer"> — </span>
+    							<a href="http://www.last.fm/music/'.$artist_name.'/_/'.$track_name.'" target="_blank">'.$track_name.'</a>
+ 	  	 				</span>
+					</td>';
+					$content .= lyric($artist_name, $track_name);			
+					$content .= play($track_name, $artist_name, $db, $method_in, "", "", "");
+					$content .= '  	  				
+  	  				<td class="list" style="padding-right:3px; min-width:360px;">
+ 	   				<span>'.$user.'</span>
+ 	           	</td>
+				</tr>';
+			if($i==0){$i++;}
+ 	     else {$i--;}
+ 	  	}	
+ 	     $place++;
+		}
+	 	$content .= '
+ 			</tbody>
+		</table>
+		</div>
+		';
+		return $content;
+	}
+
+
+?>
---
+++ include/refresh.functions.php
@@ -0,0 +1,180 @@
+<?php
+ 
+ 	function refresh($db_name, $command, $db) {
+		$getusers = $db->query("SELECT `username` FROM `ldkf_lastfm`"); 
+ 		while ($getuser = $getusers->fetch_assoc()) {
+ 			$users[]=$getuser['username'];
+ 		}		
+ 		if($db_name=="last_fm_charts_all") {
+ 			$delete = $db->query("DELETE FROM ".$db_name);	
+ 			$s=0;
+ 		}
+ 		else {
+ 			$sql = "CREATE TABLE `".$db_name."` (
+				playcount INT(3) NOT NULL,
+				artist TEXT NOT NULL COLLATE utf8_general_mysql500_ci,
+				user TEXT NOT NULL COLLATE latin1_swedish_ci
+			)";
+			if ($db->query($sql) === TRUE) {
+				$s=0;
+			}
+			else {
+   		 	echo "Error creating table: " . $db->error;
+   		 	$s=1;
+			}
+		}
+		$d=0;
+		if($s==0) {
+			foreach($users as $user_in){
+				$methode="method=".$command."&user=".$user_in;
+				$out = post($methode, $api_key);
+				if(isset($out)) {
+					$user_info_array = get_object_vars(json_decode($out));
+					if(isset($user_info_array['topartists'])) {
+						$user_info = rep(get_object_vars($user_info_array['topartists']));	
+						foreach($user_info['artist'] as $top) {
+							$info=get_object_vars($top);
+							$name=str_replace(" ", " ", $info["name"]);
+							$playcount=$info["playcount"];
+							$url=$info["url"];
+							$image = get_object_vars($info["image"][0]);
+							$image_path=$image['#text'];
+							if(!isset($image_path) or $image_path=="") {
+								$image_path="pic/empty.png";
+							}
+							$getcounter = $db->query("SELECT `playcount` FROM `".$db_name."` WHERE artist LIKE '$name'"); 
+							if(isset($getcounter->num_rows) and  $getcounter->num_rows!= 0) {
+								$counter = $getcounter->fetch_assoc()['playcount'];
+							}
+							if(isset($counter) and $counter!="") {
+								$getuser_add = $db->query("SELECT `user` FROM `".$db_name."` WHERE artist LIKE '$name' "); 
+								$user_db = $getuser_add->fetch_assoc()['user'];
+								$counter_insert=$counter+$playcount;
+								$user_insert=$user_db."&&".$user_in;
+								$update = $db->query("UPDATE `".$db_name."` SET user = '$user_insert', playcount ='$counter_insert'  where artist = '$name'");  
+							}
+							else {
+								$insert = $db->query("INSERT INTO `".$db_name."` (playcount, artist, user) VALUES ('$playcount', '$name', '$user_in')"); 
+							}
+							$counter="";
+						}
+						
+					}
+					unset($out);
+				} 
+				$d++;
+			}  
+		}
+	}
+
+#############################################################################################################################
+	function refresh2($db_name, $command, $db) {
+		$getusers = $db->query("SELECT `username` FROM `ldkf_lastfm`"); 
+ 		while ($getuser = $getusers->fetch_assoc()) {
+ 			$users[]=$getuser['username'];
+ 		}		
+ 		$sql = "CREATE TABLE `".$db_name."` (
+			playcount INT(3) NOT NULL,
+			artist TEXT NOT NULL COLLATE utf8_general_mysql500_ci,
+			user TEXT NOT NULL COLLATE latin1_swedish_ci,
+			titel TEXT NOT NULL COLLATE utf8_general_mysql500_ci
+		)";
+		if ($db->query($sql) === TRUE) {
+			$s=0;
+		}
+		else {
+   	 	echo "Error creating table: " . $db->error;
+   	 	$s=1;
+		}	
+		$d=0;
+		foreach($users as $user_in){
+			$methode="method=".$command."&user=".$user_in;
+			$out = post($methode, $api_key);
+			if(isset($out)) {
+				$user_info_array = get_object_vars(json_decode($out));
+				if(isset($user_info_array['weeklytrackchart'])) {
+					$user_info = rep(get_object_vars($user_info_array['weeklytrackchart']));	
+					foreach($user_info['track'] as $top) {
+						$info=get_object_vars($top);
+						$name=$info["name"];
+						$playcount=$info["playcount"];
+						$art_array=$info["artist"];
+						$art=get_object_vars($art_array);
+						$artist_name=$art['#text'];
+						$url=$info["url"];
+						$image = get_object_vars($info["image"][0]);
+						$image_path=$image['#text'];
+						if(!isset($image_path) or $image_path=="") {
+							$image_path="pic/empty.png";
+						}
+						$getcount = $db->query("SELECT `playcount` FROM `".$db_name."` WHERE titel LIKE '$name' "); 
+						if(isset($getcount->num_rows) and  $getcount->num_rows!= 0) {
+							$counter = $getcount->fetch_assoc()['playcount'];
+						}
+						if(isset($counter) and $counter!="") {
+							$getuser_add = $db->query("SELECT `user` FROM `".$db_name."` WHERE titel LIKE '$name' "); 
+							$user_db = $getuser_add->fetch_assoc()['user'];
+							$counter_insert=$counter+$playcount;
+							$user_insert=$user_db."&&".$user_in;
+							$update = $db->query("UPDATE `".$db_name."` SET user = '$user_insert', playcount ='$counter_insert'  where titel = '$name'");
+						}
+						else {
+							$insert = $db->query("INSERT INTO `".$db_name."` (playcount, artist, user, titel) VALUES ('$playcount', '$artist_name', '$user_in', '$name')"); 
+						}
+						$counter="";
+					}
+				}
+				unset($out);
+			}
+			$d++;
+		}  
+	}
+	
+######################################################################################################################################################
+	function refresh3($db_name, $command, $db) {
+		$getusers = $db->query("SELECT `username` FROM `ldkf_lastfm`"); 
+ 		while ($getuser = $getusers->fetch_assoc()) {
+ 			$users[]=$getuser['username'];
+ 		}	
+		$d=0;
+		$delete = $db->query("DELETE FROM ".$db_name);
+		foreach($users as $user_in){
+			$methode="method=".$command."&user=".$user_in;
+			$out = post($methode, $api_key);
+			if(isset($out)) {
+				$user_info_array = get_object_vars(json_decode($out));
+				if(isset($user_info_array['weeklytrackchart'])) {
+					$user_info = rep(get_object_vars($user_info_array['weeklytrackchart']));	
+					foreach($user_info['artist'] as $top) {
+						$info=get_object_vars($top);
+						$name=str_replace(" ", " ", $info["name"]);
+						$name_art=$info['artist'];
+						$playcount=$info["playcount"];
+						$url=$info["url"];
+						$image = get_object_vars($info["image"][0]);
+						$image_path=$image['#text'];
+						if(!isset($image_path) or $image_path=="") {
+							$image_path="pic/empty.png";
+						}
+						$getcount = $db->query("SELECT `playcount` FROM ".$db_name." WHERE titel LIKE '$name'"); 
+						$counter =	$getcount->fetch_assoc()['playcount'];
+						if(isset($counter) and $counter!="") {
+							$getuser_add = $db->query("SELECT `user` FROM ".$db_name." WHERE titel LIKE '$name' "); 
+							$user_db = $getuser_add->fetch_assoc()['user'];
+							$counter_insert=$counter+$playcount;
+							$user_insert=$user_db."&&".$user_in;
+							$update = $db->query("UPDATE ".$db_name." SET user = '$user_insert', playcount ='$counter_insert'  where titel = '$name'");
+						}
+						else {
+							$insert = $db->query("INSERT INTO ".$db_name." (playcount, artist, user, titel) VALUES ('$playcount', '$name_art', '$user_in', '$name')"); 
+						}
+						$counter="";
+					}
+				}
+				unset($out);
+			} 
+			$d++;
+		}  
+	}
+
+?>
