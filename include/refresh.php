17048077857e3365b27e9f93026acf148b0be7a0
Dominik Eichler <dspam@ldkf.de>
2016-04-17 15:11:14 +0200

    moved js function back

--- include/refresh.php
+++ include/refresh.php
@@ -9,7 +9,7 @@
 	$method_in=2;
 	$limit_in=$_POST[1];
 	if(isset($_POST[2])) {
-		$time_check=$_POST[2]
+		$time_check=$_POST[2];
 	}
 	$user=get_info("RecentTracks", $user_in, $page_in, 1, $api_key);
 	include "../user_tracks.php";	 
