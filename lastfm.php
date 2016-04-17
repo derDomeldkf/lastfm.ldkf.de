dd4bf9812deb9d6f619269f3c9255b58ab766acf
Dominik Eichler <dspam@ldkf.de>
2016-04-17 18:57:11 +0200

    updates

--- lastfm.php
+++ lastfm.php
@@ -330,32 +330,7 @@
                    $(this).removeData('bs.modal');
                });   
           });
-        	function getdata(){
-		 						setTimeout(function(){
-		 							$.post("include/refresh.php",{
-        								0: "<?php echo $user_in; ?>",
-        								1: "<?php echo $limit_in; ?>",
-        								2: $("#last").attr("title") ,
-        								3: "<?php echo $page_in; ?>",
-    								},
-   								function (data) {
-										if (data.indexOf("div") != -1) {
-											$("#last").attr("id", "");
-											$( "tr.del" ).replaceWith( "" );
-											$( "tr.repl" ).replaceWith( data );
-										}
-									}
-   							);		 		
-		 						getdata();
-		   				}, 20000);
-							}
-							$(document).ready(
-     							function(){
-           						setTimeout(function(){  
-    									getdata();
-          						}, 4000);
-						   	}
-							); 
+        	
       </script>
       <?php if($method_in==4 or ($method_in==6 and $limit_in>20) or ($method_in==7 and $limit_in>20) or ($method_in==8 and $out[1]>20) or $method_in==9 or $method_in==10): ?>
       
