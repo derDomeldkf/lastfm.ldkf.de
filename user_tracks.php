8ffd541a52c7e3d2b40cb60fb67eb94ed49aec6d
Dominik Eichler <dspam@ldkf.de>
2017-02-24 02:49:27 +0100

    lets add some js

--- user_tracks.php
+++ user_tracks.php
@@ -72,7 +72,7 @@
 				$love= $track->loved;
 				$album_decode= $track->album;
 				$track_name= $track->name;
-				$track_mbid=$track->mbid;
+				$track_mbid=$track=hash($track_name);
 				$url= $track->url;
 				$image_decode= $track->image;
 				if(isset($track->date)) {
