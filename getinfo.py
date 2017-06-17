cb8a7432eb4cd5cd8410ae62d46278773edcfecb
Dominik Eichler <dspam@ldkf.de>
2017-06-17 02:40:54 +0200

    check for utf8

--- getinfo.py
+++ getinfo.py
@@ -341,4 +341,4 @@ track() #trackdaten auslesen
 print("Alben Playtime")
 album_playtime() #trackdaten auf album hochrechnen
 print("Künstler Playtime")
-artist_playtime() #trackdaten auf künstler hochrechnen
+artist_playtime() #trackdaten auf künstler hochrechnen#
