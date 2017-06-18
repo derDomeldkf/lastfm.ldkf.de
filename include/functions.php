7f3de0c29109512ae8fe12b6113f4684f3fcd8b2
Dominik Eichler <dspam@ldkf.de>
2017-06-18 23:34:44 +0200

    fix image

--- include/functions.php
+++ include/functions.php
@@ -694,7 +694,7 @@
 					$getimage_row = $getimage->fetch_assoc();
 					var_dump($getimage_row);
 					if(!isset($getimage_row) or $getimage_row=="") {
-						$pfad="../covers/".$image_db;
+						$pfad="covers/".$image_db;
 						//echo $pfad;
 						//echo file_get_contents($images);
 						$stat=file_put_contents($pfad, file_get_contents($images))	;			
