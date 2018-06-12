af5cd7c04e7d158d709e3388f74126ad9766bd31
Dominik Eichler <dspam@ldkf.de>
2018-06-12 19:04:41 +0200

    update to post instead of get

--- include/do.php
+++ include/do.php
@@ -40,10 +40,7 @@
 	  	);
 	   $context = stream_context_create($options);
 	   $result = @file_get_contents($url, false, $context);
-	   if ($result === FALSE) {
-	   	$error=1;
-	      $result="0";
-	 	}		
+	   echo 	$result;
 		
 		
 		
