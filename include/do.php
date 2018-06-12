32ebc2282e3f7b1b4fc139af37d0e977f73acf1f
Dominik Eichler <dspam@ldkf.de>
2018-06-12 20:02:30 +0200

    update to post instead of get

--- include/do.php
+++ include/do.php
@@ -32,11 +32,11 @@
 
 	    	"format" => "json",
 		];
-	
-		$ch = curl_init('http://ws.audioscrobbler.com');
+	$methode="method=track.love&track=".$track."&artist=".$artist."&api_sig".$sig."&sk=".$sk;
+		$ch = curl_init("https://ws.audioscrobbler.com/2.0/?format=json&api_key=830d6e2d4d737d56aa1f94f717a477df&" . $methode);
 				
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		#curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 
 		
 		$response = curl_exec($ch);
