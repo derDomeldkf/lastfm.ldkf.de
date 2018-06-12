23786317c2473f78b1fb4a7ca96843c4b1076c64
Dominik Eichler <dspam@ldkf.de>
2018-06-12 19:36:29 +0200

    update to post instead of get

--- include/do.php
+++ include/do.php
@@ -37,9 +37,9 @@ $headers = [
     'Content-Type: application/x-www-form-urlencoded; charset=utf-8',
     
 ];
-
+$ch = curl_init('https://ws.audioscrobbler.com/2.0/');
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$ch = curl_init('https://ws.audioscrobbler.com/2.0/');
+		
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 
